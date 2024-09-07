c675938a4e187a82edd303e72beb7a8d8f70f035
codewithxavi <javieradrian.it@gmail.com>
2024-09-07 14:43:49 +0200

    fix next configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  assetPrefix: isProd ? "/code_repetition/" : "",
+  basePath: isProd ? "/code_repetition" : "",
+  trailingSlash: true,
+  output: "export",
+};
 
 export default nextConfig;
